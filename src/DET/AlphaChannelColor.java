71d4853f89cdbda479a69337e0c35b0f45d815ed
Khemu1 <alghost2004@gmail.com>
2023-11-25 13:02:04 +0200

    Done

---
+++ src/DET/AlphaChannelColor.java
@@ -0,0 +1,7 @@
+package DET;
+
+public class AlphaChannelColor extends Color {
+    public AlphaChannelColor() {
+        super((int)(Math.random()*10), (int)(Math.random()*10), (int)(Math.random()*10));
+    }
+}
---
+++ src/DET/Main.java
@@ -0,0 +1,9 @@
+package DET;
+
+public class Main {
+    public static void main(String[] args) {
+        AlphaChannelColor alphaChannelColor = new AlphaChannelColor();
+
+        System.out.println(alphaChannelColor.getRed() + " " + alphaChannelColor.getGreen() + " " + alphaChannelColor.getBlue());
+    }
+}
